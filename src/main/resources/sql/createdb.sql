e34de98d8401bff276b9e92df39410a08f252496
oskar123456 <xsc176@alumni.dk>
2024-04-07 17:59:23 +0200

    sql

---
+++ src/main/resources/sql/createdb.sql
@@ -0,0 +1,14 @@
+-- Database: cupcake
+
+-- DROP DATABASE IF EXISTS cupcake;
+
+CREATE DATABASE cupcake
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
